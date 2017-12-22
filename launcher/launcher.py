98d94db131a35d3878ef4361e1b7d6b44c733718
Lari Liuhamo <lari.liuhamo@gmail.com>
2017-12-22 14:11:10 +0200

    Revert "Installer and launcher fully fixed"
    This reverts commit e987bc18c794ed5785d3badda7764eef704e2e22.

--- launcher/launcher.py
+++ launcher/launcher.py
@@ -1,24 +1,21 @@
+﻿import os
 import subprocess
-from pathlib import Path
 
-curdir = Path(__file__).parent
-
-with open(curdir / "error.log", 'a') as f:
-    f.write(f"File ran!\n")
+with open("error.log", 'a') as f:
+        f.write(f"{os.getcwd()}\n")
 
 try:
-    subprocess.call([str(curdir / "pip_auto_update.bat")])
-    with open(curdir / "error.log", 'a') as f:
+    subprocess.call(["pip_auto_update.bat"])
+    with open("error.log", 'a') as f:
         f.write("Auto-updater found and executed\n")
-
-    filepath = curdir.parent / 'src' / 'main.py'
-    with open(curdir / "error.log", 'a') as f:
-        f.write(f"{filepath}\n")
-    subprocess.call(['python.exe', str(filepath)], cwd=str(filepath.parent))
-
+    #os.chdir("..\\src")
+    filepath = os.path.join(os.getcwd(), "..", "src", "main.py")
+    subprocess.call([filepath])
+    
 except Exception as e:
-    with open(curdir / "error.log", 'a') as f:
+    with open("error.log", 'a') as f:
         f.write(f"{e}\n")
-
-with open(curdir / "error.log", 'a') as f:
+        
+with open("error.log", 'a') as f:
     f.write(f"\n{'='*20}\n\n")
+    
