e987bc18c794ed5785d3badda7764eef704e2e22
Lari Liuhamo <lari.liuhamo@gmail.com>
2017-12-22 14:09:10 +0200

    Installer and launcher fully fixed

--- launcher/installer.bat
+++ launcher/installer.bat
@@ -7,9 +7,20 @@ chcp 65001
 rem Define current directory path
 set mypath=%~dp0
 
-where /q python
-
+rem Create install directory, if not present
+set install_dir="%systemdrive%\larin_ohjelmat\youtube-dl-gui"
+echo %install_dir%
+mkdir "%install_dir%"
+mkdir "%install_dir%\src\dl"
 if ERRORLEVEL 1 (
+    echo.
+    ver > nul
+)
+
+rem Ensure that Python is installed on the system, and prompt installation if needed
+where /q python
+if ERRORLEVEL 1 (
+    rem If Python isn't installed/found on PATH
     echo Larin videolataaja vaatii Python-tulkin version 3.6 tai uudemman. Asennathan sen ensin.
     echo Asennuksen aikana tulee eteen pieni ruutu jonka vieressä lukee "Include in PATH" tai jotain. Varmista että tässä ruudussa on oikeinmerkki!
     echo Sen voi ladata osoitteesta: https://www.python.org/downloads/
@@ -18,6 +29,8 @@ if ERRORLEVEL 1 (
 ) else (
     echo Python-asennus havaittu. Mainiota!
     echo.
+
+    rem Install the winshell module, which is required for installation
     call pip install winshell
     if ERRORLEVEL 1 (
         echo Virhe asennettaessa Python-moduulia 'winshell'
@@ -46,24 +59,16 @@ if ERRORLEVEL 1 (
     echo.
 )
 
-set install_dir="%systemdrive%\larin_ohjelmat\youtube-dl-gui"
-echo %install_dir%
-mkdir "%install_dir%"
-mkdir "%install_dir%\src\dl"
-if ERRORLEVEL 1 (
-    echo.
-    ver > nul
-)
-
+rem Install the bloody program already!
 xcopy /s /e /y /v /k /g "%mypath:~0,-1%"\..\* "%install_dir%"
 if ERRORLEVEL 1 (
-    echo Problem!
+    echo Virhe asennettaessa! Pyydä Laria apuun.
     echo %ERRORLEVEL%
 ) else (
-    echo No problems detected.
+    echo Asennus suoritettu onnistuneesti
 )
 
-rem Create desktop shortcuts
+rem Create desktop shortcuts for launcher and the video download directory
 call python -c "import os, winshell; from win32com.client import Dispatch; desktop=winshell.desktop();path=os.path.join(desktop, 'Larin videolataaja.lnk'); wDir=r'C:\larin_ohjelmat\youtube-dl-gui\launcher'; target=f'{wDir}\\launcher.py'; target2=f'{wDir}\\src\\dl'; icon=f'{wDir}\\launcher.py'; shell=Dispatch('WScript.Shell'); shortcut=shell.CreateShortcut(path); shortcut.Targetpath=target; shortcut.WorkingDirectory=wDir; shortcut.IconLocation=icon; shortcut.save(); shortcut2=shell.CreateShortcut(path); shortcut2.Targetpath=target2; shortcut2.WorkingDirectory=f'{wDir}\\src\\dl'; shortcut2.save()"
 if ERRORLEVEL 1 (
     echo Ongelma luodessa pikakuvakkeita!
@@ -72,6 +77,11 @@ if ERRORLEVEL 1 (
 )
 
 echo Asennus valmis, käynnistetään ohjelmaa...
-start "%install_dir%\launcher\launcher.py"
+call python "%install_dir%\launcher\launcher.py"
+if ERRORLEVEL 1 (
+    echo Ongelma havaittu!
+) else (
+    echo Kaikki kuten pitääkin!
+)
 
 exit /b 0
--- launcher/launcher.py
+++ launcher/launcher.py
@@ -1,21 +1,24 @@
-﻿import os
 import subprocess
+from pathlib import Path
 
-with open("error.log", 'a') as f:
-        f.write(f"{os.getcwd()}\n")
+curdir = Path(__file__).parent
+
+with open(curdir / "error.log", 'a') as f:
+    f.write(f"File ran!\n")
 
 try:
-    subprocess.call(["pip_auto_update.bat"])
-    with open("error.log", 'a') as f:
+    subprocess.call([str(curdir / "pip_auto_update.bat")])
+    with open(curdir / "error.log", 'a') as f:
         f.write("Auto-updater found and executed\n")
-    #os.chdir("..\\src")
-    filepath = os.path.join(os.getcwd(), "..", "src", "main.py")
-    subprocess.call([filepath])
-    
+
+    filepath = curdir.parent / 'src' / 'main.py'
+    with open(curdir / "error.log", 'a') as f:
+        f.write(f"{filepath}\n")
+    subprocess.call(['python.exe', str(filepath)], cwd=str(filepath.parent))
+
 except Exception as e:
-    with open("error.log", 'a') as f:
+    with open(curdir / "error.log", 'a') as f:
         f.write(f"{e}\n")
-        
-with open("error.log", 'a') as f:
+
+with open(curdir / "error.log", 'a') as f:
     f.write(f"\n{'='*20}\n\n")
-    
