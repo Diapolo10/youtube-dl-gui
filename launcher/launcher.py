50dfbfb0b5f5d49ff43634c150922ba45d48ac97
Lari Liuhamo <lari.liuhamo@gmail.com>
2017-12-22 18:39:31 +0200

    Version checking implemented

--- launcher/launcher.py
+++ launcher/launcher.py
@@ -1,18 +1,37 @@
 ﻿import os
+import sys
 import subprocess
+import base64
+import requests
 from pathlib import Path
 
 curdir = Path(__file__).parent
+rootdir = curdir.parent
 
 try:
+    # Update all pip packages
     subprocess.call([str(curdir / "pip_auto_update.bat")])
-    #os.chdir("..\\src")
-    #filepath = os.path.join(os.getcwd(), "..", "src", "main.py")
-    filepath = curdir.parent / "src" / "main.py"
-    subprocess.call(["python", str(filepath)], cwd=str(filepath.parent))
+
+    # Check version
+    url = r"https://raw.githubusercontent.com/Diapolo10/youtube-dl-gui/nightly/VERSION"
+    req = requests.get(url)
+    github_version = req.text.strip()
+    with open(rootdir / "VERSION") as f:
+        local_version = f.read().strip()
+
+    if local_version == github_version:
+        pass
+    else:
+        pass #TODO: Update repository
+
+    # Launch the program
+    filepath = rootdir / "src" / "main.py"
+    subprocess.Popen(["python", str(filepath)], cwd=str(filepath.parent))
 
 except Exception as e:
     with open("error.log", 'a') as f:
         f.write(f"{curdir}\n")
         f.write(f"{e}\n")
         f.write(f"\n{'='*20}\n\n")
+
+sys.exit()
